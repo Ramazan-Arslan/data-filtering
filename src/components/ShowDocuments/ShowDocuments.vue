ee9c1b79cfb73694cf9af31ccc78a74b6b7ddd00
Ramazan Arslan <ramazan@decktopus.com>
2022-12-25 00:04:27 +0300

    add bootstrap lib and create new component

---
+++ src/components/ShowDocuments/ShowDocuments.vue
@@ -0,0 +1,3 @@
+<template src="./ShowDocuments.html"></template>
+<script src="./ShowDocuments.ts" lang="ts"></script>
+<style src="./ShowDocuments.scss" lang="scss"></style>
--- src/views/HomeView/HomeView.ts
+++ src/views/HomeView/HomeView.ts
@@ -1,5 +1,10 @@
 import { Component, Vue } from 'vue-property-decorator';
-@Component({})
+import ShowDocuments from '@/components/ShowDocuments/ShowDocuments.vue';
+@Component({
+  components: {
+    ShowDocuments,
+  },
+})
 export default class HomeView extends Vue {
   protected mounted() {
     console.log('mounted');
